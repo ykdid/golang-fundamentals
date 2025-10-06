563783610fca2e14ca6a09c62c8bb68484ef8750
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2025-10-06 15:57:44 +0300

    learned constant variables in go

---
+++ basics/constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func ConstantsDemo() {
+
+	fmt.Println("Constants Demo:")
+
+	const Pi = 3.14159265458979323846264338327950288
+	const AppName = "Yusuf's Go Journay"
+
+	fmt.Println(Pi, AppName)
+}
--- basics/main.go
+++ basics/main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 
 	VariablesDemo()
+	ConstantsDemo()
 }
