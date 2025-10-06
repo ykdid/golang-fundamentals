5cbcdcba9993ea81f6a3adff813186985f67c50f
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2025-10-06 16:00:54 +0300

    learned iota for const

--- basics/constants.go
+++ basics/constants.go
@@ -10,4 +10,21 @@ func ConstantsDemo() {
 	const AppName = "Yusuf's Go Journay"
 
 	fmt.Println(Pi, AppName)
+
+	const (
+		Monday = 1
+		Sunday = 2
+		Tuesday = 3
+	)
+
+	fmt.Println(Monday, Sunday, Tuesday)
+
+	//iota auto iterate like enum 0,1,2,..,n
+	const (
+		Red = iota
+		White
+		Black
+	)
+
+	fmt.Println("Colors:",Red, White, Black)
 }
