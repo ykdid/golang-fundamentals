3939236e3ac3518f5d76fe39255019b136aef4f9
Yusuf KAYA <135675262+ykdid@users.noreply.github.com>
2025-10-11 03:00:44 +0300

    interface example

---
+++ structs-and-methods/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Quadrilateral struct {
+	Width, Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Quadrilateral) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Quadrilateral) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func PrintShapeInfo(s Shape) {
+	fmt.Printf("Area: %.2f\n", s.Area())
+	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
+	fmt.Println("-----")
+}
--- structs-and-methods/main.go
+++ structs-and-methods/main.go
@@ -5,4 +5,10 @@ func main() {
 	StructsDemo()
 	MethodsDemo()
 	EmbeddingDemo()
+	
+	quad := Quadrilateral{Width: 5, Height: 3}
+	circ := Circle{Radius: 4}
+
+	PrintShapeInfo(quad)
+	PrintShapeInfo(circ)
 }
